716f302c377da9c8cacfa5952a1bf5318f386fbb
Anton Bobryshev <tosh.blogs@gmail.com>
2022-05-27 22:25:02 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Антон Бобрышев on 27.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
